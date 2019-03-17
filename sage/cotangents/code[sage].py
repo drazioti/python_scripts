b2aa9d6cc380f19b6406e56b5fdee24d1754e8d0
draziotis <drazioti@gmail.com>
2019-03-17 20:27:05 +0200

    create cotangent directory and some images were added

---
+++ sage/cotangents/code[sage].py
@@ -0,0 +1,24 @@
+"""
+We provide a simple script to compute cotangents sums and generate images.
+
+for instance the following code uses k = 601  (prime)and generates points (h/600,f(h/600))
+where f (h/k) = sum ( i/k * cot(pi*i*h/600) , m =1...k-1)
+
+M=[];L=[]
+import time
+k = 601
+A = time.time()
+L = [[h/k,cotangent(h,k).n()] for h in range(1,k) if cotangent(h,k)<>0 ]
+B = time.tiime()-A
+print "time passed:",B
+
+Use the code as you like.
+"""
+
+def cotangent(h,k):
+    var('m')
+    if gcd(h,k)==1:
+        S = - np.sum(m/k * cot(pi*m*h/k),m,1,k-1)
+        return S
+    else:
+        return 0
