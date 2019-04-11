76253dcc9384f36ce40a63254faadff7c7736223
draziotis <drazioti@gmail.com>
2019-04-12 02:48:00 +0300

    mp3

---
+++ various/youtube_to_mp3/myscript.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+import sys
+sys.path.insert(0,'/home/draziotis/youtube-dl')
+import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+fo = open("list.txt", "r")
+lines = fo.readlines()
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    for line in lines:
+        ydl.download([line])
+
