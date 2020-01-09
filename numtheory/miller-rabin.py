018f62a8fa1da32d838e9cad05f9e37969475487
draz <drazioti@gmail.com>
2020-01-09 03:39:08 +0200

    update miller_rabin to work for large numbers, using gmpy2.

--- numtheory/miller-rabin.py
+++ numtheory/miller-rabin.py
@@ -1,15 +1,35 @@
+''' 
+
+Miller - Rabin.
+Works for large numbers.
+We need gmpy2 library. Tested in python 2. 
+
+>>>miller_rabin(2**1024-1,8)
+>>>'composite'
+
+'''
+
+
+
 def miller_rabin(n,k):
     
     import random
     import math
+    import gmpy2
+    from gmpy2 import mpz,mpq,mpfr,mpc
+
+    
 
     def find_s_t(n):
+        n = mpz(n)
         # here we have to compute the order_2 of n
-        i = 1
+        i = mpz(1)
         isint = 0
         while isint == 0:
             
-            isint = math.floor( (n-1)/(2.**i)) - math.ceil( (n-1)/(2.**i))
+            isint = gmpy2.powmod(n-1,1,2**i)
+            #print isint
+            #isint = gmpy2.floor( (n-1)/(2.**i)) - gmpy2.ceil( (n-1)/(2.**i))
             # sos : the variable isint does not work for large n, say 2048 bits
             # TODO :  fix it
             
@@ -25,7 +45,7 @@ def miller_rabin(n,k):
     s,t = find_s_t(n)
     while i<=k:
         a = random.randint(2,n-1)
-        b = a**t % n
+        b =  gmpy2.powmod(a,t,n)
         if b == 1:
             j = j + 1
             if j == k:
@@ -35,6 +55,6 @@ def miller_rabin(n,k):
                 j = j + 1
                 if j == k:
                     return "n is strong probable prime"
-            b = (b ** 2) % n
+            b =  gmpy2.powmod(b,2,n)
         i = i + 1
     return "composite"
