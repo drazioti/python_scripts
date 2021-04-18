5d2d1eab41c23694d0837f0d21c477481a482656
drazioti <drazioti@gmail.com>
2021-04-19 01:14:54 +0300

    Update experiments.py

--- paper_dsa/experiments.py
+++ paper_dsa/experiments.py
@@ -1,15 +1,16 @@
 '''
 This experiment studies how many digital signatures we need to collect
 in order to take derivative ephemeral keys with prescribed number of bits.
-In our case the prime q has 160 bits and we need 206 signatures such that their
-ephemeral keys have 160-bits. We see that on average we need about 1500 signatures
-to collect.
+In our case the prime q has 160 bits and we need 204 signatures such that their
+ephemeral keys have 160-bits. We see that on average we need to collect about 1500 signatures.
+Now providing special type of primes q, for instance having more MSB, we see that, we need (on average)
+<< 1500 signatures.
 
 sage:ell = 160
 sage:number_of_desired_signatures = 204
 sage:number_of_instances = 100
 sage:mean([samples(ell,number_of_desired_signatures) for i in range(number_of_instances)])
-1406
+1406 # this is the average
 '''
 
 
@@ -34,7 +35,7 @@ def r(q,k):
     return int(ZZ.random_element( 2^(k-1), q));
 
 def samples(ell,number_of_signatures):
-    q = genq(ell) # generate a prime having ell b its
+    q = genq(ell) # generate a prime having ell bits
     c = 0
     R = 0
     while c<number_of_signatures:
