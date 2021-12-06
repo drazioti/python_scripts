6c429b1a1be382c671ccd5082369e4e8f86a228b
drazioti <drazioti@gmail.com>
2021-12-06 14:21:13 +0200

    Update divisors_with_known_prime_factorization.py

--- numtheory/divisors_with_known_prime_factorization.py
+++ numtheory/divisors_with_known_prime_factorization.py
@@ -9,8 +9,8 @@ def find_divisors_from_primes(r,H):
     Then this function will return all the positive divisors of
     N, exploiting that we know all the primes dividing N. 
     e.g.
-    r = [2,3,5,7]
-    H = [2,1,1,1]
+    r = [2,3,5,7] #primes
+    H = [2,1,1,1] #exponents
     C=find_divisors_from_primes(r,H)
     print C
 
