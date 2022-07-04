ac50381ed3d819189c737d6eab1e30d2e23c4f8a
drazioti <drazioti@gmail.com>
2022-07-04 15:09:26 +0300

    Update main.py

--- various/excel/main.py
+++ various/excel/main.py
@@ -2,102 +2,128 @@
 # author : K.A.Draziotis
 # Licence : GPL
 # dependencies : 
-# numpy,pandas,xlrd
+# pip3 install numpy
+# pip3 install pandas
 # pip3 install xlrd
+# pip3 install xlsxwriter
+# pip3 install pandas-ods-reader
+# pip3 install openpyxl
 
 '''
 Example
 
-You have to hardcode the paths of the excel files. Here we denote them as A.xlsx and B.xls
-Also you have to hardcode NROWS.
+You have to hardcode the paths of the xlsx files. Here we denote them as A.xlsx and B.xls
 
 $python3 main.py
+
 In practice: 
-1. Download the grade file from the old sis, say B.xls
-2. sort with respect to aem and save it (for easy crosscheck).
-3. Save it with the name B_.xls
-4. [hardcode in the code] Set as 'input' the file from elearning [First column aem and second column grades]
-5. set as 'output' the file B_.xlsx
+1. Download the grade file from the old sis, say B.xlsx
+2. sort with respect to aem and save it.
+3. Save with the name B_.xlsx
+4. set input the file from *elearning*
+This file must have two columns, where in the first row we have 'AEM' and the first row,second column 'Βαθμός'
+5. set output the file     *B_.xlsx*
 6. run python3 main.py
-7. copy the column 'βαθμός' from B_.xlsx
-8. paste it to the column 'βαθμός' of B.xlsx and save it.
+7. get the column 'βαθμός' from B_.xlsx
+8. paste it to the column 'βαθμός' of B.xlsx
 9. end
 '''
 
 import pandas as pd
 import xlsxwriter
-from pandas_ods_reader import read_ods
 import numpy as np
+from pandas_ods_reader import read_ods
 
-counter = 0    # counts the number of grades successfully passed, to the second excel
-input_path  = 'A.xlsx' # the file from elearning
-output_path = 'B_.xls'      # the official grade sheet 
+def checkIfDuplicates(listOfElems):
+    ''' Check if given list contains any duplicates '''
+    if len(listOfElems) == len(set(listOfElems)):
+    	print("there are no duplicates")
+    else:
+    	print("[+]there ARE duplicates")
 
+
+
+counter = 0    # counts the number of grades successfully passed to the second excel
+input_path  = 'grades_ma_2.xlsx'
+output_path = '2021-2022_ΙΟΥΝΙΟΣ_NCO-02-01_ΜΑΘΗΜΑΤΙΚΗ ΑΝΑΛΥΣΗ ΙΙ_.xlsx'
+
+A1 = []
 A = []
 B = []
 
 # the follow two lines must be hardcoded
 
 SKIPROWS = 0    # depends on the format of the file A.xlsx : input path (the header does not count)
-#NROWS   = 296  # TODO : equals to : [number of non null rows] - [SKIPROWS+1]
 NROWS1   = 1000 # a large number
 
-#first  = pd.read_excel(input_path,skiprows=SKIPROWS,nrows=NROWS1,usecols='A:B',index_col=None,sheet_name='Sheet2') # if you use many sheets
-first  = pd.read_excel(input_path,skiprows=SKIPROWS,nrows=NROWS1,usecols='A:B',index_col=None)
-second = pd.read_excel(output_path,index_col=None)
+first  = pd.read_excel(input_path, skiprows=SKIPROWS, nrows=NROWS1, usecols='A:B', index_col=None, sheet_name='0')
+second = pd.read_excel(output_path, index_col=None)
 
 NROWS = first.count(axis=0)[1] # counts without the headers
-print(NROWS) # please check with the real number
-#print(q)
+print("Rows:",NROWS) # please check with the real number
 
 #1. take from 'first' the pair first [AEM,Σύνολο]
 
 for i in range(0,NROWS):
-	A.append(list(first.xs(i)))
+	A1.append(list(first.xs(i)))
+
+# since the first coordinate is float i.e. AEM is considered here float, convert it to integer
+for x in A1:
+    A.append([int(x[0]),x[1]])
 print(A)
-S = np.sum([int(A[i][1]) for i in range(len(A)) ])
 		
 #2. find 'ΑΕΜ' in second and add Σύνολο to Βαθμός
 #   if there is not such aem print that this aem does not exist.
 
 print("\n")
 for i in range(len(A)):	
-	test_value = A[i][0]
+	test_value = int(A[i][0])
 	grade      = A[i][1]
-	output     = second.loc[second[' ΑΕΜ '] == test_value] # for the old sis
 	
-	#output     = second.loc[second['ΑΕΜ'] == test_value] # for the new sis
+	output     = second.loc[second['ΑΕΜ'] == test_value] 
 	if len(output) == 0:
 		print("the student with AEM:",test_value,"does not exist")
 		B.append([test_value,grade])
 	else:
+		# bug: δεν ελεγχει διπλοεγγραφές
 		counter = counter + 1
 		found_the_row = output.index[0]
 		#print("line:",found_the_row)
-		#print("βαθμός:",A[i][1]) # for old sis
 		second.at[found_the_row,' Βαθμός ']=grade
 		#print(second.iloc[found_the_row])
-		#print(second.at[found_the_row,' ΑΕΜ ']) # for the old sis
 		#print(second.at[found_the_row,'ΑΕΜ']) # for the new sis
 
 
-C = [] # C contains the valid grades >=5
-B1 = [x[0] for x in B]
+c = 0
+C = []
+tens  = 0
+nines = 0
 for i in range(len(A)):
-	if A[i][0] not in  B1:
-		if int(A[i][1])>=5:
-			C.append(A[i][1])
+	if int(A[i][1])>=5:
+		c+=1
+		C.append(A[i][1])
+	if int(A[i][1])==10:
+		tens+=1
+	if int(A[i][1])>=9 and int(A[i][1])<10:
+		nines+=1
+
+S = np.sum([C[i] for i in range(len(C)) ])
+
+
+check_list = []
+check_list = [z[0] for z in A] # take a list with AEM only
+checkIfDuplicates(check_list)
 
 print("the number of grades are:",len(A))
-print("the number of valid grades are:",len(A)-len(B))
 print("successes:",counter)
 print("fails:",len(A)-counter)
 print(B)
-S1 = np.sum([int(B[i][1]) for i in range(len(B)) ])
-print("\n",">=5 :",len(C))
-print(" <5  :",len(A)-len(B)-len(C))
-print(" mean (of the valid AEMs):",(S-S1)/counter)
-print(" success rate:",len(C)/counter*100,"%")
+print("\n",">=5 :",c)
+print(" <5  :",counter-c)
+print(" mean:",S/c)
+print(" success rate:",100*c/counter)
+print(" tens:",tens)
+print(" >=9 and <10:",nines)
 
 #3. convert 'second' to xlsx.
 
