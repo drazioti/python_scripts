c5d8b4c541879faa7168aa40038dccbdbed3c218
drazioti <drazioti@gmail.com>
2023-02-26 15:56:41 +0200

    Create print_module_licenses.sh

---
+++ various/print_module_licenses.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+number_of_modules=$(lsmod|wc -l)
+a=$(lsmod |awk '{print $1}'|awk 'NR>1')
+
+for mods in $a
+do         
+    b=$(/sbin/modinfo $mods|grep license|awk '{print $2$3$4$5}'|awk 'NR<=1')
+    c=$(/sbin/modinfo $mods|grep description|awk '{print $2$3$4$5}'|awk 'NR<=1')
+    printf -- '%-35s %-35s %-5s %-s\n\n' "$mods" "$b" "$c">&2   
+done | column -t
+printf -- 'you have: %d kernel modules. \n' "$number_of_modules" >&2
